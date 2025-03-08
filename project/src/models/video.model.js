4ce0523423d2d7dfe53d753637dd7f55aed73e6b
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-03-08 11:34:49 +0000

    add mongooseAggregation plugin to video model

--- project/src/models/video.model.js
+++ project/src/models/video.model.js
@@ -1,5 +1,5 @@
 import { model, Schema } from "mongoose";
-
+import mongooseAggregatePaginate  from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema({
     title:{
         type:String,
@@ -35,5 +35,6 @@ const videoSchema = new Schema({
     }
 }, {timestamps:true})
 
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = model("Video", videoSchema)
