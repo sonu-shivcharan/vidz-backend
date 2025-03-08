0923bab84981117a9da2aaed9c46bd14f1b05918
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-03-08 11:34:10 +0000

    add methods to uesrSchema

--- project/src/models/user.model.js
+++ project/src/models/user.model.js
@@ -1,5 +1,6 @@
 import { model, Schema } from "mongoose"
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const userSchema = new Schema({
     username: {
         type: String,
@@ -16,7 +17,7 @@ const userSchema = new Schema({
         trim: true,
         lowercase: true,
     },
-    fullname:{
+    fullName:{
         type: String,
         required: true,
         trim: true,
@@ -40,5 +41,47 @@ const userSchema = new Schema({
         type:String,
     }
 }, { timestamps: true })
+
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        this.password = bcrypt(this.password)
+    }
+    next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName : this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+            algorithm:"RS256"
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
+            algorithm:"RS256"
+        }
+    )
+}
+
 
 export const User = model("User", userSchema);
