6d68e4b593e9cb3265e25e14c888e9ddefd75be3
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-03-10 11:07:27 +0000

    config cloudinary.js

---
+++ project/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+async function uploadFileToCloudinary(pathToFile){
+    try {
+        if(!pathToFile) return null;
+
+        const response = await cloudinary.uploader.upload(pathToFile, {
+            resource_type:"auto",
+        })
+        console.log("File uploaded to cloudinary succsfully ", response.url);  
+        return response
+          
+    } catch (error) {
+        fs.unlinkSync(pathToFile);
+        console.log("Error uploding file to Cloudinary uploadFileToCloudinary Error::", error);
+        return null;
+    }
+}
+
+
+export {uploadFileToCloudinary};
