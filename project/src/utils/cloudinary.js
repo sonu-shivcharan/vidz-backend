909ad3b5600063917a9babdb8a158fe9682e1c75
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-03-24 11:31:24 +0000

    add delete utility

--- project/src/utils/cloudinary.js
+++ project/src/utils/cloudinary.js
@@ -1,17 +1,15 @@
 import {v2 as cloudinary } from "cloudinary"
 import fs from "fs"
+import ApiError from "./apiError.js";
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret:process.env.CLOUDINARY_API_SECRET
 });
 
-async function uploadFileToCloudinary(pathToFile){
-
-    
+async function uploadFileToCloudinary(pathToFile){  
     try {
         if(!pathToFile) return null;
-
         const response = await cloudinary.uploader.upload(pathToFile, {
             resource_type:"auto",
         })
@@ -27,4 +25,15 @@ async function uploadFileToCloudinary(pathToFile){
 }
 
 
-export {uploadFileToCloudinary};
+async function deleteFileFromCloudinary(fileId) {
+    try{
+        if(!fileId) return null;
+        const response = await cloudinary.uploader.destroy(fileId);
+        return response;
+    }catch(error){
+        console.log("Error deleting file from cloudinary", error);
+        throw new ApiError(500, error?.message || "Somthing went wrong while deleting file from cloudinary")
+    }
+}
+
+export {uploadFileToCloudinary, deleteFileFromCloudinary};
