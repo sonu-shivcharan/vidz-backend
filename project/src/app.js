2ed85ce8b759e571d7b8d149c6ccffb3481ace63
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-04-11 11:05:28 +0000

    feat: implement video upload and management routes with controller functions

--- project/src/app.js
+++ project/src/app.js
@@ -17,9 +17,10 @@ app.use(cookieParser())
 
 // import routes
 import userRouter from "./routes/user.route.js"
-
+import videoRouter from "./routes/video.route.js"
 app.use("/api/v1/users", userRouter)
 
+app.use("/api/v1/videos", videoRouter)
 
 
 export default app;
---
+++ project/src/controllers/video.controller.js
@@ -0,0 +1,83 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Video} from "../models/video.model.js"
+import {User} from "../models/user.model.js"
+import ApiError from "../utils/apiError.js"
+import ApiResponse from "../utils/apiResponse.js"
+import {uploadFileToCloudinary} from "../utils/cloudinary.js"
+import asyncHandler from "../utils/asyncHandler.js"
+
+
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    //TODO: get all videos based on query, sort, pagination
+
+})
+
+const publishAVideo = asyncHandler(async (req, res) => {
+    const { title, description} = req.body
+    if(!title.trim()){
+        throw new ApiError(400, "Video title is required.")
+    }else if(!description.trim()){
+        throw new ApiError(400,"Video description is required.")
+    }
+    const files = req.files;
+    const videoLocalPath =(files?.videoFile?.length > 0 ) ? files.videoFile[0].path : "";
+    if(!videoLocalPath){
+        throw new ApiError(400, "Video file is required.")
+    }
+
+    const thumbnailLocalPath = (files?.thumbnail?.length>0) ? files.thumbnail[0].path:"";
+    if(!thumbnailLocalPath){
+        throw new ApiError(400, "Video thumbnail is required.")
+    }   
+    const videoFile = await uploadFileToCloudinary(videoLocalPath);
+    const thumbnailFile = await uploadFileToCloudinary(thumbnailLocalPath)
+    const userId = String(req.user._id) //converting user id explicitly to string
+
+    const video = await Video.create({
+        title:title.trim(),
+        description:description.trim(),
+        videoFile:videoFile.url,
+        thumbnail:thumbnailFile.url,
+        duration:videoFile.duration,
+        owner: new mongoose.Types.ObjectId(userId),
+        // mongoose.Types.ObjectId(id:number)->deprecated -> that why I converted userId explicity to string
+    })
+    console.log('video', video)
+    if(!video){
+        throw new ApiError(500, "Something went wrong while publshing video", video)
+    }
+   console.log('video after uploading it cloudinary', video )
+    return res.status(200).json({data:"Testing video upload route ", title, description})
+})
+
+const getVideoById = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    const video = await Video.findById(videoId)
+
+    return res.status(200).json(new ApiResponse(200, video, "Video"))
+})
+
+const updateVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    //TODO: update video details like title, description, thumbnail
+
+})
+
+const deleteVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    //TODO: delete video
+})
+
+const togglePublishStatus = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+})
+
+export {
+    getAllVideos,
+    publishAVideo,
+    getVideoById,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus
+}
