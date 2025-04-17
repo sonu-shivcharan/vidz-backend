f775ac411563d5d980a8d2463152977eb5936b1a
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-04-17 09:20:22 +0000

    feat: implement pagination for subscriber retrieval and refactor subscription routes

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -28,7 +28,6 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         )
       );
   }
-
   try {
     await Subscription.create({ subscriber: userId, channel: channelId });
     return res
@@ -51,53 +50,122 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
   if (!isValidObjectId(channelId)) {
     throw new ApiError(400, "Invalid channel id");
   }
- try {
-     const subscribers = await Subscription.aggregate([
-       {
-         $match: {
-           channel : new mongoose.Types.ObjectId(String(channelId))
-         },
-       },
-       {
-         $lookup: {
-           from: "users",
-           localField: "subscriber",
-           foreignField: "_id",
-           as: "subscribers",
-           pipeline: [
-             {
-               $project: {
-                 username: 1,
-                 fullName: 1,
-                 email: 1,
-                 avatar: 1,
-                 coverImage: 1,
-               },
-             },
-           ],
-         },
-       },
-       {
-         $addFields: {
-           subscribersCount: { $size: "$subscribers" },
-         },
-       },
-     ]);
-     return res.status(200).json(new ApiResponse(200, subscribers, "Fetched subscribers successfully"))
- } catch (error) {
-    throw new ApiError(
-        error?.statusCode || 500,
-        error?.message || "Something went wrong while toggling the subscription"
+  try {
+    const subscribers = await Subscription.aggregatePaginate(
+      [
+        {
+          $match: {
+            channel: new mongoose.Types.ObjectId(String(channelId)),
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "subscriber",
+            foreignField: "_id",
+            as: "subscriber",
+            pipeline: [
+              {
+                $project: {
+                  _id: 1,
+                  username: 1,
+                  fullName: 1,
+                  email: 1,
+                  avatar: 1,
+                  coverImage: 1,
+                },
+              },
+            ],
+          },
+        },
+        {
+          $addFields: {
+            subscriber: { $first: "$subscriber" },
+            subscribedAt: "$createdAt",
+          },
+        },
+        {
+          $unset: ["channel", "updatedAt", "createdAt"],
+        },
+      ],
+      { page: page, limit: limit }
+    );
+
+    if (subscribers) {
+      subscribers["subscribers"] = subscribers["docs"];
+      delete subscribers["docs"];
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, subscribers, "Fetched subscribers successfully")
       );
- }
+  } catch (error) {
+    throw new ApiError(
+      error?.statusCode || 500,
+      error?.message || "Something went wrong while toggling the subscription"
+    );
+  }
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+  console.log("subscriberId", subscriberId);
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid subscriber id");
+  }
+
+  try {
+    const channels = await Subscription.aggregate([
+      {
+        $match: {
+          subscriber: new mongoose.Types.ObjectId(String(subscriberId)),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "channel",
+          foreignField: "_id",
+          as: "channel",
+          pipeline: [
+            {
+              $project: {
+                _id: 1,
+                username: 1,
+                fullName: 1,
+                avatar: 1,
+                email: 1,
+                avatar: 1,
+                coverImage: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          channel: { $first: "$channel" },
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { channels }, "Channel fetched successfully.")
+      );
+  } catch (error) {
+    throw new ApiError(
+      error?.statusCode || 500,
+      error?.message || "Something went wrong while toggling the subscription"
+    );
+  }
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,5 @@
 import {Schema, model} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const subscriptionSchema = new Schema({
     subscriber:{
@@ -13,4 +14,5 @@ const subscriptionSchema = new Schema({
     timestamps: true
 })
 
+subscriptionSchema.plugin(mongooseAggregatePaginate)
 export const Subscription = model("Subscription", subscriptionSchema)
