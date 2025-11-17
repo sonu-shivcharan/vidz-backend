a67492985f4b6cbcfcb1d80efbd7d7df15f0d25a
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-17 22:32:48 +0530

    feat: add morgan for logging HTTP requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-
+import morgan from "morgan"
 const app = express();
+
+app.use(morgan("dev"))
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
