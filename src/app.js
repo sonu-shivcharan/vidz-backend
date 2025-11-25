0cb377456bf6a18622f8252dc471d778c0ecad80
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-25 21:52:35 +0530

    style: format code for consistency and readability

--- src/app.js
+++ src/app.js
@@ -1,52 +1,51 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import morgan from "morgan"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 const app = express();
-const MODE = process.env.NODE_ENV == "development" ? "dev" : "tiny"
+const MODE = process.env.NODE_ENV == "development" ? "dev" : "tiny";
 
-app.use(morgan(MODE))
-app.use(cors({
-    origin:process.env.CORS_ORIGIN?.split(","),
-    credentials:true
-}))
+app.use(morgan(MODE));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(","),
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // import routes
-import userRouter from "./routes/user.route.js"
-import videoRouter from "./routes/video.route.js"
-import subscriptionRouter from "./routes/subscription.route.js"
-import commentRouter from "./routes/comment.route.js"
-import likeRouter from "./routes/like.route.js"
-import dashboardRouter from "./routes/dashboard.route.js"
-import playlistRouter from "./routes/playlist.route.js"
-import tweetRouter from "./routes/tweet.route.js"
-import healthcheckRouter  from "./routes/healthcheck.route.js";
+import userRouter from "./routes/user.route.js";
+import videoRouter from "./routes/video.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import commentRouter from "./routes/comment.route.js";
+import likeRouter from "./routes/like.route.js";
+import dashboardRouter from "./routes/dashboard.route.js";
+import playlistRouter from "./routes/playlist.route.js";
+import tweetRouter from "./routes/tweet.route.js";
+import healthcheckRouter from "./routes/healthcheck.route.js";
 import ApiResponse from "./utils/apiResponse.js";
 
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/tweets", tweetRouter);
 
 // health check
 
-app.use("/api/v1/health",healthcheckRouter);
-
+app.use("/api/v1/health", healthcheckRouter);
 
 app.get("/", (req, res) => {
-
-    return res.status(200).json(new ApiResponse(200, {status:"OK"}, "Server is up and running"));
-
-})
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { status: "OK" }, "Server is up and running"));
+});
 export default app;
