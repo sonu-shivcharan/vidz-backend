2a65f85c8bb8dd378928f79e6900e0e0afa7c170
sonu-shivcharan <sonushivcharan@outlook.com>
2026-02-17 16:14:28 +0530

    just for testing the deployment on ec2

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,9 +1,12 @@
-import asyncHandler from "../utils/asyncHandler.js"; 
+import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/apiResponse.js";
 const healthcheck = asyncHandler(async (_req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-    return res.status(200).json(new ApiResponse(200, {status:"OK"}, "Server is up and running"));
-})
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { status: "OK" }, "Server is up and running broo")
+    );
+});
 
-export {healthcheck}    
-    
+export { healthcheck };
